6283cde2347a0fbfa0fa7c17f4ce980ba3b22476
Spencer Liechty <spencerliechty@gmail.com>
2014-06-27 18:06:26 -0500

    Add detail and index templates

---
+++ counter/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from counter import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<event_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<event_id>\d+)/reset/$', views.reset, name='reset'),
+)
