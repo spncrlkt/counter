8ecc13ed10ab8c4be09a8868090e04c5fcca5fdc
Spencer Liechty <spencerliechty@gmail.com>
2014-06-29 22:15:37 -0500

    Add CSRF token to reset buttons

--- static/js/counter_ajax.js
+++ static/js/counter_ajax.js
@@ -2,7 +2,19 @@ $(document).ready(function () {
     $('button.reset').click(function() {
         var that =$(this);
         var event_id = that.attr('data');
-        $.getJSON('/counter/'+event_id+'/reset', function(data) {
+        var csrf_token = that.attr('csrf_token');
+        var reset_url = '/counter/'+event_id+'/reset/';
+        $.ajax({
+            beforeSend: function(xhr, settings) {
+                if (!this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrf_token);
+                }
+            },
+            type: "POST",
+            url: reset_url,
+            data: {'event_id':event_id},
+        })
+         .done(function(data) {
             //$('#timer').show();
             $('#updated').show().fadeOut();
         });
